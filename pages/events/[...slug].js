7dea31888efcc4e51729d8be95b433fa7374ad0c
somidi85 <somidi@eliya.ch>
2023-12-24 16:38:09 +0100

    search form and event list page for sub slugs (up to video 167)

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,7 +1,47 @@
-import React from "react";
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
 
-const FilteredEventsPage = () => {
-  return <div>EventDetailSlug</div>;
-};
+function FilteredEventsPage() {
+  const router = useRouter();
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  // convert to numerical
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter!</p>;
+  }
+
+  return (
+    <div>
+      <EventList items={filteredEvents} />
+    </div>
+  );
+}
 
 export default FilteredEventsPage;
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,34 @@
-import React from "react";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
 
-const EventDetailPage = () => {
-  return <div>EventDetailPage</div>;
-};
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
+function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
+}
 
 export default EventDetailPage;
